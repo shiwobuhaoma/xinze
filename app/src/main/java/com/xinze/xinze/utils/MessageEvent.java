e07e0c3bf2ff2b77eaac14d9ad538cdadf3cbf5e
lxf <1009281661@qq.com>
2018-06-08 15:24:03 +0800

    1、增加eventbus

---
+++ app/src/main/java/com/xinze/xinze/utils/MessageEvent.java
@@ -0,0 +1,18 @@
+package com.xinze.xinze.utils;
+
+public class MessageEvent {
+    private String message;
+
+    public MessageEvent(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
